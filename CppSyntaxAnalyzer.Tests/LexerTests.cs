a97e02a257e93a5ce5b3125f544e9222af96a22f
Владислав Гладкий <vlad.v.gladkiy@gmail.com>
2026-03-29 22:12:49 +0300

    change structure and add testing project with lexer tests

---
+++ CppSyntaxAnalyzer.Tests/LexerTests.cs
@@ -0,0 +1,62 @@
+﻿using Xunit;
+using CppSyntaxAnalyzer;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace CppSyntaxAnalyzer.Tests;
+
+public class LexerTests
+{
+    // Parameterized test
+    [Theory]
+    [InlineData("int", TokenKind.Keyword)]
+    [InlineData("return", TokenKind.Keyword)]
+    [InlineData("42", TokenKind.IntLiteral)]
+    [InlineData("3.14", TokenKind.FloatLiteral)]
+    [InlineData("++", TokenKind.Operator)]
+    [InlineData("my_var", TokenKind.Identifier)]
+    [InlineData("{", TokenKind.Punctuator)]
+    public void GetNextToken_SingleLexeme_ReturnCorrectKind(string source, TokenKind expectedKind)
+    {
+        var lexer = new Lexer(source);
+        var token = lexer.GetNextToken();
+
+        Assert.Equal(expectedKind, token.Kind);
+        Assert.Equal(source, token.Lexeme);
+    }
+
+    // Complex Assert (collections)
+    [Fact]
+    public void TokenizeAll_SimpleStatement_ReturnsCorrectTokenSequence()
+    {
+        var source = "int x = 10;";
+        var lexer = new Lexer(source);
+
+        var tokens = lexer.TokenizeAll();
+
+        Assert.NotNull(tokens);
+        Assert.Collection(tokens,
+            t => { Assert.Equal(TokenKind.Keyword, t.Kind); Assert.Equal("int", t.Lexeme); },
+            t => { Assert.Equal(TokenKind.Identifier, t.Kind); Assert.Equal("x", t.Lexeme); },
+            t => { Assert.Equal(TokenKind.Operator, t.Kind); Assert.Equal("=", t.Lexeme); },
+            t => { Assert.Equal(TokenKind.IntLiteral, t.Kind); Assert.Equal("10", t.Lexeme); },
+            t => { Assert.Equal(TokenKind.Operator, t.Kind); Assert.Equal(";", t.Lexeme); },
+            t => { Assert.Equal(TokenKind.EndOfFile, t.Kind); }
+        );
+    }
+
+    //Complex Assert (lines)
+    [Fact]
+    public void TokenizeAll_UnterminatedString_TerunsErrorTokenWithMessage()
+    {
+        var source = "\"unterminated";
+        var lexer = new Lexer(source);
+
+        var tokens = lexer.TokenizeAll();
+        var errorToken = tokens.First();
+
+        Assert.Equal(TokenKind.Error, errorToken.Kind);
+
+        Assert.StartsWith("Unterminated", errorToken.Message);
+    }
+}
---
+++ CppSyntaxAnalyzer.Tests/ParserTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CppSyntaxAnalyzer.Tests
+{
+    internal class ParserTests
+    {
+    }
+}
